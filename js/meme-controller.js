1ee1d17f8e10efdf83a31d9609106367eeb6a3b2
Yoni Lazarus <ylazarus@gmail.com>
2022-02-16 11:22:22 +0200

    routine commit

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,38 +1,29 @@
-'use strict'
+"use strict";
 
+var gCanvas;
+var gCtx;
 
-
-
-function onInit(){
-    console.log('hi!');
+function onInit() {
+  gCanvas = document.getElementById("my-canvas");
+  gCtx = gCanvas.getContext("2d");
+  renderMeme();
 }
 
+function renderMeme() {
+  // var { img } = getCanvas()
+  var img = new Image();
+  img.onload = () => {
+    gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height);
+    drawText("hello world", 250, 250);
+  };
+  img.src = "images/2.jpg";
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function drawText(text, x, y) {
+  gCtx.lineWidth = 1;
+  gCtx.strokeStyle = "black";
+  gCtx.fillStyle = "blue";
+  gCtx.font = "50px Arial";
+  gCtx.fillText(text, x, y);
+  gCtx.strokeText(text, x, y);
+}
---
+++ js/services/meme-service.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var gMeme;
+
+
+
+
+
+
+function getMeme() {
+  return gMeme;
+}
